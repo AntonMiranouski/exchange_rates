70c36975638bbbbcd52f86931fe4f911880c146c
AntonMiranouski <51458394+AntonMiranouski@users.noreply.github.com>
2020-07-25 13:47:53 +0300

    Delete ExRatesController.d.ts

--- .sfdx/typings/lwc/apex/ExRatesController.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/ExRatesController.getRates" {
-  export default function getRates(param: {startDate: any, endDate: any, baseCurrency: any}): Promise<any>;
-}
-declare module "@salesforce/apex/ExRatesController.getCurrenciesPickList" {
-  export default function getCurrenciesPickList(param: {objectType: any, selectedField: any}): Promise<any>;
-}
